9ddbe83ad516571b32616d48b2521a9686927432
MUSA CHIRIMWAMI Gloire <chirimwami2014@gmail.com>
2020-03-25 07:23:38 +0100

    git et instance

---
+++ instance/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = '9462bfc3ca8d37b136173798873d05ea'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='gloire', server='localhost', database='based')
+#SQLALCHEMY_DATABASE_URI = "mysql://200181:Esperance1993@@mysql-chirimwami.alwaysdata.net/chirimwami_choeurpilote"
